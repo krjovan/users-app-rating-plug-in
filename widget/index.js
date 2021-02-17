b95ee73bac99026f9ca00cf63badeb5713cb02d6
krjovan <jovankron@gmail.com>
2021-02-17 21:25:01 +0100

    Widget user interactive rating completed

--- widget/index.js
+++ widget/index.js
@@ -1,3 +1,7 @@
+let myRating = {
+    rating: 0
+};
+
 function load() {
     buildfire.datastore.get("settings", (err, response) => {
         if (err)
@@ -16,20 +20,50 @@ function load() {
             return console.error(err);
         }
         else
-           loadUserRating(user);
+            loadUserRating();
     });
 
     authManager.enforceLogin();
 
 }
 
-function loadUserRating(user) {
+function loadUserRating() {
+    buildfire.userData.get('myRating', function (err, response) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if (response.data != null || response.data.rating != null) {
+            document.getElementById('print').innerHTML = response.data.rating;
+            icons = document.getElementById('iconsDiv').getElementsByTagName('i');
+            for (var i = 0; i < icons.length; i++) {
+                if (i < response.data.rating) {
+                    icons[i].style.color = 'orange';
+                }
+                else {
+                    icons[i].style.color = 'grey';
+                }
+
+            }
+        }
+    });
+}
+
+function saveUserRating(rating) {
+    myRating.rating = parseInt(rating.id);
+    buildfire.userData.save(myRating, "myRating", function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            loadUserRating();
+        }
+    });
 }
 
 function applySettings(data) {
-    console.log(data);
-    icons =  document.getElementById('iconsDiv').getElementsByTagName('i');
-    for (var i=0; i < icons.length; i++) {
+    icons = document.getElementById('iconsDiv').getElementsByTagName('i');
+    for (var i = 0; i < icons.length; i++) {
         icons[i].className = data.icon;
     }
 }
